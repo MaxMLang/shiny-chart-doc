b322a579cc31020a1c485a1e1fb99686c4861f62
MaxMLang <langmaxmelchior@gmail.com>
2023-09-11 23:33:27 +0200

    Added code examples to rank and correlation chapters

--- tabs/ui/correlation.R
+++ tabs/ui/correlation.R
@@ -12,19 +12,51 @@ correlation <- tabPanel(title = "Korrelation",
                       column(1, img(src='img/correlation/scatter.png', align = "left", height="150%", width="150%")),
                       column(5, "Die Standardmethode zur Darstellung der Beziehung zwischen zwei metrischen Variablen, von denen jede eine eigene Achse hat.")
                     ),
+                    br(),
+                    strong("Code Beispiele"),
+                    br(),
+                    fluidRow(column(1, HTML('<a href="https://r-charts.com/correlation/scatter-plot-ggplot2/"><img src="img/R.png" title="R Logo" width="40"  /></a>')),
+                             column(1, HTML('<a href="https://www.w3schools.com/python/python_ml_scatterplot.asp"><img src="img/python.png" title="Python" width="40"  /></a>'))),
+                    
+                    
+                    
                     h4("Säulen + Linien Zeitstrahl"),
                     fluidRow(
                       column(1, img(src='img/correlation/column_line.png', align = "left", height="150%", width="150%")),
                       column(5, "Eine gute Möglichkeit, die Beziehung zwischen einem Betrag (Spalten) und einem Kurs (Linie) darzustellen.")
                     ),
+                    br(),
+                    strong("Code Beispiele"),
+                    br(),
+                    fluidRow(column(1, HTML('<a href="https://www.geeksforgeeks.org/combine-bar-and-line-chart-in-ggplot2-in-r/"><img src="img/R.png" title="R Logo" width="40"  /></a>')),
+                             column(1, HTML('<a href="https://www.tutorialspoint.com/how-to-show-a-bar-and-line-graph-on-the-same-plot-in-matplotlib"><img src="img/python.png" title="Python" width="40"  /></a>'))),
+                    
+                    
+                    
+                    
                     h4("Blasendiagramm"),
                     fluidRow(
                       column(1, img(src='img/correlation/bubble.png', align = "left", height="150%", width="150%")),
                       column(5, "Wie ein Streudiagramm, aber mit zusätzlichen Details, indem die Größe der Kreise anhand einer dritten Variable bestimmt wird.")
                     ),
+                    br(),
+                    strong("Code Beispiele"),
+                    br(),
+                    fluidRow(column(1, HTML('<a href="https://r-charts.com/correlation/bubble-chart-ggplot2/"><img src="img/R.png" title="R Logo" width="40"  /></a>')),
+                             column(1, HTML('<a href="https://plotly.com/python/bubble-charts/"><img src="img/python.png" title="Python" width="40"  /></a>'))),
+                    
                     h4("XY Heatmap"),
                     fluidRow(
                       column(1, img(src='img/correlation/xy_heatmap.png', align = "left", height="150%", width="150%")),
                       column(5, "Eine gute Möglichkeit, die Muster zwischen 2 Datenkategorien zu zeigen, weniger effektiv bei der Darstellung feiner Unterschiede in den Beträgen.")
-                    )
+                    ),
+                    br(),
+                    strong("Code Beispiele"),
+                    br(),
+                    fluidRow(column(1, HTML('<a href="https://r-charts.com/correlation/heat-map-ggplot2/"><img src="img/R.png" title="R Logo" width="40"  /></a>')),
+                             column(1, HTML('<a href="https://python-graph-gallery.com/heatmap/"><img src="img/python.png" title="Python" width="40"  /></a>'))),
+                    
+                    
+                    
+                    
 )
--- tabs/ui/rank.R
+++ tabs/ui/rank.R
@@ -13,34 +13,82 @@ rank <- tabPanel(title = "Rangliste",
                    column(1, img(src='img/rank/barA.png', align = "left", height="150%", width="150%")),
                    column(5, "Standard-Balkendiagramme zeigen die Rangfolge der Werte viel einfacher an, wenn sie in eine Reihenfolge gebracht werden.")
                  ),
+                 br(),
+                 strong("Code Beispiele"),
+                 br(),
+                 fluidRow(column(1, HTML('<a href="https://statisticsglobe.com/ordered-bars-in-ggplot2-barchart-in-r"><img src="img/R.png" title="R Logo" width="40"  /></a>')),
+                          column(1, HTML('<a href="https://www.tutorialspoint.com/how-to-sort-bars-in-increasing-order-in-a-bar-chart-in-matplotlib"><img src="img/python.png" title="Python" width="40"  /></a>'))),
+                 
+                 
                  h4("Geordnetes Balkendiagramm (Säulen)"),
                  fluidRow(
                    column(1, img(src='img/rank/barB.png', align = "left", height="150%", width="150%")),
                    column(5, "Siehe oben")
                  ),
+                 br(),
+                 strong("Code Beispiele"),
+                 br(),
+                 fluidRow(column(1, HTML('<a href="https://statisticsglobe.com/ordered-bars-in-ggplot2-barchart-in-r"><img src="img/R.png" title="R Logo" width="40"  /></a>')),
+                          column(1, HTML('<a href="https://www.tutorialspoint.com/how-to-sort-bars-in-increasing-order-in-a-bar-chart-in-matplotlib"><img src="img/python.png" title="Python" width="40"  /></a>'))),
+                 
+                 
                  h4("Geordnetes proportionales Symbol"),
                  fluidRow(
                    column(1, img(src='img/rank/size_symbol.png', align = "left", height="150%", width="150%")),
                    column(5, "Wird verwendet, wenn große Unterschiede zwischen den Werten bestehen und/oder es nicht so wichtig ist, die feinen Unterschiede zwischen den Daten zu erkennen.")
                  ),
+                 br(),
+                 strong("Code Beispiele"),
+                 br(),
+                 fluidRow(column(1, HTML('<a href="https://plotly.com/r/bubble-charts/"><img src="img/R.png" title="R Logo" width="40"  /></a>')),
+                          column(1, HTML('<a href="https://plotly.com/python/bubble-charts/"><img src="img/python.png" title="Python" width="40"  /></a>'))),
+                 
+                 
+                 
                  h4("Einachsiges Streudiagramm (Dot strip plot)"),
                  fluidRow(
                    column(1, img(src='img/rank/dotstrip.png', align = "left", height="150%", width="150%")),
                    column(5, "Die Reihenfolge der Punkte auf einem Streifen ist eine raumeffiziente Methode, um Ranglisten über mehrere Kategorien hinweg zu erstellen.")
                  ),
+                 br(),
+                 strong("Code Beispiele"),
+                 br(),
+                 fluidRow(column(1, HTML('<a href="https://homepage.divms.uiowa.edu/~luke/classes/STAT4580/stripplot.html"><img src="img/R.png" title="R Logo" width="40"  /></a>')),
+                          column(1, HTML('<a href="https://seaborn.pydata.org/generated/seaborn.stripplot.html"><img src="img/python.png" title="Python" width="40"  /></a>'))),
+                 
+                 
+                 
                  h4("Steigung"),
                  fluidRow(
                    column(1, img(src='img/rank/slope.png', align = "left", height="150%", width="150%")),
                    column(5, "Perfekt, um zu zeigen, wie sich die Ränge im Laufe der Zeit verändert haben oder zwischen den Kategorien variieren.")
                  ),
+                 br(),
+                 strong("Code Beispiele"),
+                 br(),
+                 fluidRow(column(1, HTML('<a href="https://r-charts.com/evolution/newggslopegraph/"><img src="img/R.png" title="R Logo" width="40"  /></a>')),
+                          column(1, HTML('<a href="https://towardsdatascience.com/slope-charts-with-pythons-matplotlib-2c3456c137b8"><img src="img/python.png" title="Python" width="40"  /></a>'))),
+                 
                  h4("Lollipop Diagramm"),
                  fluidRow(
                    column(1, img(src='img/rank/lollipop.png', align = "left", height="150%", width="150%")),
                    column(5, "Lollipops lenken die Aufmerksamkeit stärker auf den Datenwert als Standardbalken/-spalten und können auch Rang und Wert getrennt anzeigen.")
                  ),
+                 br(),
+                 strong("Code Beispiele"),
+                 br(),
+                 fluidRow(column(1, HTML('<a href="https://r-charts.com/ranking/lollipop-chart-ggplot2/"><img src="img/R.png" title="R Logo" width="40"  /></a>')),
+                          column(1, HTML('<a href="https://python-graph-gallery.com/180-basic-lollipop-plot/"><img src="img/python.png" title="Python" width="40"  /></a>'))),
+                 
                  h4("Liniendiagramm (Bump)"),
                  fluidRow(
                    column(1, img(src='img/rank/bump.png', align = "left", height="150%", width="150%")),
                    column(5, "Effektiv für die Darstellung sich ändernder Rankings über mehrere Daten hinweg. Bei großen Datensätzen sollten Sie eine farbliche Gruppierung der Zeilen in Betracht ziehen.")
-                 )
+                 ),
+                 br(),
+                 strong("Code Beispiele"),
+                 br(),
+                 fluidRow(column(1, HTML('<a href="https://r-charts.com/ranking/ggbump/"><img src="img/R.png" title="R Logo" width="40"  /></a>')),
+                          column(1, HTML('<a href="https://altair-viz.github.io/gallery/bump_chart.html"><img src="img/python.png" title="Python" width="40"  /></a>'))),
+                 
 )
